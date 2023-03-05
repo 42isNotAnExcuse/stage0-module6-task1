6201912f264698bfafb57a8227d0103ff4edf064
igorbugaiov <proxipixel@gmail.com>
2023-03-05 19:58:18 +0100

    st0-mod6-t1 1st-try

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -2,4 +2,50 @@ package com.epam.mjc.stage0;
 
 public class Animal {
 
+    public static void main(String[] args) {
+        System.out.println(new Bird().getDescription());
+        System.out.println(new Dog().getDescription());
+    }
+
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
+
+    public Animal(String color, int numOfPaws, boolean isFur) {
+        this.color = color;
+        this.numberOfPaws = numOfPaws;
+        this.hasFur = isFur;
+    }
+
+    public void setNumberOfPaws(int nop) {
+        numberOfPaws = nop;
+    }
+
+    public int getNumberOfPaws() {
+        return numberOfPaws;
+    }
+
+    public void setColor(String c) {
+        color = c;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setHasFur(boolean gf) {
+        hasFur = gf;
+    }
+
+    public boolean getHasFur() {
+        return hasFur;
+    }
+
+    public String getDescription() {
+        String paws = numberOfPaws > 1 ? "paws" : "paw";
+        String furOrNot = hasFur ? "a" : "no";
+
+        return String.format("This animal is mostly %s. It has %d %s and %s fur.",
+                color, numberOfPaws, paws, furOrNot);
+    }
 }
